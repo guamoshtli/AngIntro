99d1f0a0cf895df3bd7060e733b4317d2fc4387f
gonzalo <gonzalo@mail.com>
2023-07-10 13:10:37 -0600

    String Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements AfterViewInit {
   parentMessage: string ='Message coming from parent component, changed';
   message: string = '';
   fromChildOutput: string = '';
+  messageStr: string = 'Messag from TypeScript Component File';
 
   //@ViewChild(PostComponent)  childComp?: any;
   @ViewChild(PostComponent) childComponent!: PostComponent;
