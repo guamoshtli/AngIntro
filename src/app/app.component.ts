6c7845935936886d633169cab09cc2111449ab91
gonzalo <gonzalo@mail.com>
2023-07-10 13:44:05 -0600

    Class Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements AfterViewInit {
   fromChildOutput: string = '';
   messageStr: string = 'Messag from TypeScript Component File';
   imgUrl: string = 'https://rdironworks.com/wp-content/uploads/2017/12/dummy-200x200.png';
+  bool: boolean = true;
 
   //@ViewChild(PostComponent)  childComp?: any;
   @ViewChild(PostComponent) childComponent!: PostComponent;
